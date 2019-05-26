7f12b36a8277ba9abb6e2ca8356a2abdc550fc9f
Robert Kędryna <robert.kedryna@gmail.com>
2019-05-27 00:04:36 +0200

    I've made a sample scene cobbled together from some free assets. I've almost made a mini quick-time game, but I had problems with collision detection, check the QuickTime script.

---
+++ Banterion/Assets/Scripts/QuickTime.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuickTime : MonoBehaviour
+{
+    public Image SpriteUp;
+    public Image SpriteDown;
+    public Image SpriteLeft;
+    public Image SpriteRight;
+    public Image SpriteBG;
+    public GameObject car;
+
+    //hide the buttons
+    public void Start()
+    {
+        SpriteUp.enabled = false;
+        SpriteDown.enabled = false;
+        SpriteLeft.enabled = false;
+        SpriteRight.enabled = false;
+        SpriteBG.enabled = false;
+    }
+
+    //detect cars in the a hitch area, does not currently work
+    private void OnTriggerEnter(Collider other)
+    {
+
+        print("Is anything even happening?");
+
+        //unity is being a collision-bitch as always, here lays the problem
+        if (other.gameObject.tag == "Car")
+        {
+            SpriteBG.enabled = true;
+            Getcombination();
+            
+        }
+    }
+
+    //get a random int to determine what button appears and what button the player has to press.
+    public void Getcombination()
+    {
+        int n = Random.Range(0, 4);
+
+        if (n == 0)
+        {
+            SpriteUp.enabled = true;
+            
+            if (Input.GetButtonDown("up"))
+            {
+                SpriteUp.enabled = false;
+                SpriteBG.enabled = false;
+                return;
+            }
+        }
+
+       else if (n == 1)
+        {
+            SpriteDown.enabled = true;
+
+            if (Input.GetButtonDown("down"))
+            {
+                SpriteDown.enabled = false;
+                SpriteBG.enabled = false;
+                return;
+            }
+        }
+
+       else  if (n == 2)
+       {
+            SpriteLeft.enabled = true;
+
+            if (Input.GetButtonDown("left"))
+            {
+                SpriteLeft.enabled = false;
+                SpriteBG.enabled = false;
+                return;
+            }
+       }
+
+        else if (n == 3)
+        {
+            SpriteRight.enabled = true;
+
+            if (Input.GetButtonDown("right"))
+            {
+                SpriteRight.enabled = false;
+                SpriteBG.enabled = false;
+                return;
+            }
+        }
+
+    }
+
+}

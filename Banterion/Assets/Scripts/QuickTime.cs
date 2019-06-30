0266bfa7140a992fddb92b271516173a27ec4f3b
Saul <p_lison@protonmail.com>
2019-06-30 15:42:42 +0200

    Car deployment + collisions (no QTE yet)
    changed car deployment method:
        - modified prefab
        - script instead of animation for movement
        - spawn randomly (0.5 s - 3.2 s or something like that)
    
    collisions are recorded, but QTE doesn't work yet

--- Banterion/Assets/Scripts/QuickTime.cs
+++ Banterion/Assets/Scripts/QuickTime.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class QuickTime : MonoBehaviour
-{
+public class QuickTime : MonoBehaviour {
     public Image SpriteUp;
     public Image SpriteDown;
     public Image SpriteLeft;
@@ -13,8 +12,7 @@ public class QuickTime : MonoBehaviour
     public GameObject car;
 
     //hide the buttons
-    public void Start()
-    {
+    public void Start() {
         SpriteUp.enabled = false;
         SpriteDown.enabled = false;
         SpriteLeft.enabled = false;
@@ -23,67 +21,52 @@ public class QuickTime : MonoBehaviour
     }
 
     //detect cars in the a hitch area, does not currently work
-    private void OnTriggerEnter(Collider other)
-    {
+    private void OnTriggerEnter2D(Collider2D other) {
 
-        print("Is anything even happening?");
+        SpriteBG.enabled = true;
+        Getcombination();
 
-        //unity is being a collision-bitch as always, here lays the problem
-        if (other.gameObject.tag == "Car")
-        {
-            SpriteBG.enabled = true;
-            Getcombination();
-            
-        }
+
     }
 
     //get a random int to determine what button appears and what button the player has to press.
-    public void Getcombination()
-    {
+    public void Getcombination() {
         int n = Random.Range(0, 4);
 
-        if (n == 0)
-        {
+        if (n == 0) {
             SpriteUp.enabled = true;
-            
-            if (Input.GetButtonDown("up"))
-            {
+
+            if (Input.GetButtonDown("up")) {
                 SpriteUp.enabled = false;
                 SpriteBG.enabled = false;
                 return;
             }
         }
 
-       else if (n == 1)
-        {
+        else if (n == 1) {
             SpriteDown.enabled = true;
 
-            if (Input.GetButtonDown("down"))
-            {
+            if (Input.GetButtonDown("down")) {
                 SpriteDown.enabled = false;
                 SpriteBG.enabled = false;
                 return;
             }
         }
 
-       else  if (n == 2)
-       {
+        else if (n == 2) {
             SpriteLeft.enabled = true;
 
-            if (Input.GetButtonDown("left"))
-            {
+            if (Input.GetButtonDown("left")) {
                 SpriteLeft.enabled = false;
                 SpriteBG.enabled = false;
                 return;
             }
-       }
+        }
 
-        else if (n == 3)
-        {
+        else if (n == 3) {
             SpriteRight.enabled = true;
 
-            if (Input.GetButtonDown("right"))
-            {
+            if (Input.GetButtonDown("right")) {
                 SpriteRight.enabled = false;
                 SpriteBG.enabled = false;
                 return;
@@ -91,5 +74,4 @@ public class QuickTime : MonoBehaviour
         }
 
     }
-
 }

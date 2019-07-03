d2817f8e9c30d43209413cf3ce567842e0e57a77
Robert Kędryna <robert.kedryna@gmail.com>
2019-07-03 21:27:45 +0200

    chmura

---
+++ Banterion/Assets/Shadow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Cloud")
+        {
+            //InvokeRepeating(Frustration.Rest(5,false),1,3);
+        }
+    }
+
+}
